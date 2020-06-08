e1c16552d6b7c1f03599b427e361c0f7c5a69f74
deepak <dmaddukuri@gmail.com>
2020-06-08 10:36:50 -0400

    Pushing all latest changes to git

---
+++ detail/jquery.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	dots:false,
+	autoplay:true,
+	
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:10
+        },
+        1000:{
+            items:15
+        }
+    }
+})
+
---
+++ listing/jquery2.js
@@ -0,0 +1,20 @@
+$('.owl-carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	dots:false,
+	autoplay:true,
+	
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
